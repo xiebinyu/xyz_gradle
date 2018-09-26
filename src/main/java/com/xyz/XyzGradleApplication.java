82e3a6f6b8f355f2d8882cc8ca9f98d19ae49d64
xiebinyu <xiebinyu@xdf.cn>
2018-09-26 09:40:57 +0800

    init

---
+++ src/main/java/com/xyz/XyzGradleApplication.java
@@ -0,0 +1,12 @@
+package com.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzGradleApplication.class, args);
+	}
+}
